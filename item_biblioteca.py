cfc40f0424b62b68cb246284cabf23f46dedc469
Yasmin Nathaly <Ynathaly58@gmail.com>
2026-03-02 13:06:31 -0300

    Atualizando

--- item_biblioteca.py
+++ item_biblioteca.py
@@ -1,6 +1,8 @@
-class item_biblioteca:
+class item_biblioteca: #Criei uma superclasse chamada item_biblioteca
     def __init__(self, codigo, titulo, ano):
         self.__codigo = codigo
         self.__titulo = titulo
         self.__ano = ano
         self.disponivel = True
+
+#Adicionando métodos obrigatórios
