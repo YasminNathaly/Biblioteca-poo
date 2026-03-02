64dc0f061f219c4c803a0d850681e7a9f59d54aa
Yasmin Nathaly <Ynathaly58@gmail.com>
2026-03-02 13:00:16 -0300

    Subindo item_biblioteca

---
+++ item_biblioteca.py
@@ -0,0 +1,6 @@
+class item_biblioteca:
+    def __init__(self, codigo, titulo, ano):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.disponivel = True
