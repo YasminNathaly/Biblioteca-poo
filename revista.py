8f5ca415b2f4764e494282550d961805605a262d
Yasmin Nathaly <Ynathaly58@gmail.com>
2026-03-04 10:08:52 -0300

    Ultima alteração revista.py

--- revista.py
+++ revista.py
@@ -5,3 +5,11 @@ class Revista(ItemBiblioteca):
         super().__init__(codigo, titulo, ano)
         self.__edicao = edicao
         self.__mes = mes
+
+    def exibir_detalhes(self):
+        print(f"codigo: {self.get_codigo()}")
+        print(f"titulo: {self.get_titulo()}")
+        print(f"ano: {self.get_ano()}")
+        print(f"edicao: {self.get_edicao()}")
+        print(f"mes: {self.get_mes()}")
+        print(f"disponivel: {self.get_disponivel()}")
