28e038cc4461e089ef1e939a01bba00231836533
Yasmin Nathaly <Ynathaly58@gmail.com>
2026-03-04 10:34:05 -0300

    Ultima atualização-biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -6,4 +6,30 @@ class Biblioteca:
         self.__itens.append(item)
         print(f"item: '{item.get_titulo()}'Adicionado com sucesso!")
 
+    def listar_itens(self):
+        if not self.__itens:
+            print("Nenhum item cadastrado.")
+        else:
+            for item in self.__itens:
+                item.exibir_detalhes()
+
+    def buscar_item_por_codigo(self, codigo):
+        item = self.buscar_item_por_codigo(codigo):
+        for item in self.__itens:
+            if item.get_codigo() == codigo:
+                return item
+            return None
         
+    def  emprestar_item(self, codigo):
+        item = self.buscar_por_codigo(codigo)
+        if item:
+            item.emprestar()
+        else:
+            print(f"item com codigo{codigo} não encontrado.")
+
+    def devolver_item(self, codigo):
+        item = self.buscar_por_codigo(codigo)
+        if item:
+            item.devolver()
+        else:
+            print(f"Item com codigo{codigo}não encontrado.")
